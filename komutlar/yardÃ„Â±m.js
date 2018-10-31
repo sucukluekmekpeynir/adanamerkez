5cde6853b05de3a0b7da76e75530372e19a0e16f
sucukluekmekpeynir <44277051+sucukluekmekpeynir@users.noreply.github.com>
2018-10-31 23:00:58 +0300

    Add files via upload

--- komutlar/yardÃ„Â±m.js
+++ komutlar/yardÃ„Â±m.js
@@ -5,12 +5,12 @@ var prefix = ayarlar.prefix;
 
 exports.run = (client, message, params) => {
   const embedyardim = new Discord.RichEmbed()
-  .setTitle("Komut Listesi")
+  .setTitle("Kralbot Komut Listesi")
   .setDescription('')
-  .setColor(0x00ffff)
-      .setDescription('**•** dve!eğlence = Eğlence komutları\n**•** dve!kişisel = Kişisel komutlar\n**•** dve!yetkili = Yetkili komutları\n**•** dve!oyun = Oyun komutları\n**•** dve!müzik = Müzik komutları\n**•** dve!destek = Destek komutları')
+  .setColor(random)
+      .setDescription('**•** k?eğlence = Eğlence komutları\n**•** k?kişisel = Kişisel komutlar\n**•** k?yetkili = Yetkili komutları\n**•** k?oyun = Oyun komutları\n**•** k?müzik = Müzik komutları\n**•** k?destek = Destek komutları')
       .addField("» Linkler", `[Bot Davet Linki](https://discordapp.com/api/oauth2/authorize?client_id=498573744030351377&permissions=2146958583&scope=bot)` + "**\n**"+`[Destek Sunucusu](https://discord.gg/gwmBhyV)`, false)
-      .setFooter('Örnek kullanım: dve!eğlence')
+      .setFooter('Örnek kullanım: k?eğlence')
 
   if (!params[0]) {
     const commandNames = Array.from(client.commands.keys());
@@ -20,7 +20,7 @@ exports.run = (client, message, params) => {
     let command = params[0];
     if (client.commands.has(command)) {
       command = client.commands.get(command);
-      message.author.send('asciidoc', `= ${command.help.name} = \n${command.help.description}\nDoğru kullanım: ` + prefix + `${command.help.usage}`);
+      message.author.send('asciidoc', `= ${command.help.name} = \n${command.help.description}\nDoğru kullanım: k? `${command.help.usage}`);
     }
   }
 };
