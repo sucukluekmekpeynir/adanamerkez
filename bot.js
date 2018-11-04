bb2fcfb4ccd5c733668b6c6186eadc2deba87d7e
sucukluekmekpeynir <44277051+sucukluekmekpeynir@users.noreply.github.com>
2018-11-04 18:21:09 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -384,15 +384,6 @@ description: ('Bu kanal NSFW kanalı değil.')
  }
 }
 });
-
-client.on("message", msg => {
-
-    const kufur = ["discordapp", ".com", ".net", ".xyz", ".tk", "gulu", ".pw", ".io", ".me", ".gg", "www.", "https", "http", ".gl"];
-    if (kufur.some(word => msg.content.includes(word)) ) {
-        msg.delete()
-        msg.reply("Reklam Yapma Kardeşim Seni Yetkililere Söyledim :angry: :negative_squared_cross_mark: ")
-    }
-});
  
     client.on('message', async msg => {
     if (msg.content.toLowerCase() === prefix + "disko") {
