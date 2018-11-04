c890993974e196327964c7a9fb32d3fdea15dde2
sucukluekmekpeynir <44277051+sucukluekmekpeynir@users.noreply.github.com>
2018-11-04 16:45:14 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -385,4 +385,14 @@ description: ('Bu kanal NSFW kanalı değil.')
 }
 });
 
+client.on("message", msg => {
+
+    const kufur = ["discordapp", ".com", ".net", ".xyz", ".tk", "gulu", ".pw", ".io", ".me", ".gg", "www.", "https", "http", ".gl"];
+    if (kufur.some(word => msg.content.includes(word)) ) {
+        msg.delete()
+        msg.reply("Reklam yapma krdşm")
+    }
+});
+ 
+
 client.login(process.env.BOT_TOKEN);
