2b821d90bc9af3bb6c67531246682d11fdf976a8
sucukluekmekpeynir <44277051+sucukluekmekpeynir@users.noreply.github.com>
2018-11-04 17:21:27 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -394,5 +394,14 @@ client.on("message", msg => {
     }
 });
  
+    client.on('message', async msg => {
+    if (msg.content.toLowerCase() === prefix + "disko") {
+   if (msg.channel.type === "dm") return;
+  const rol = 'Disko' // Rol ismi buraya
+  setInterval(() => {
+      msg.guild.roles.find(s => s.name === rol).setColor("RANDOM")
+      }, 9000);
+  }
+});
 
 client.login(process.env.BOT_TOKEN);
